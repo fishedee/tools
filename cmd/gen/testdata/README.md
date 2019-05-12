1ebff44a6a51feb40f3f0b355a00285a0dbf78e1
jdlau <jdlau@qq.com>
2019-05-12 14:22:06 +0800

    cmd/gen/testdata: 添加更多测试并添加README.md记录基准测试结果

---
+++ cmd/gen/testdata/README.md
@@ -0,0 +1,38 @@
+# 测试结果
+
+可以看到，生成的方法和手写的方法性能相差无几。
+
+```go
+go test -v -bench=. -benchmem
+
+goos: linux
+goarch: amd64
+pkg: github.com/donnol/tools/cmd/gen/testdata
+
+BenchmarkQueryColumnHand-8 300000 5001 ns/op 8192 B/op 1 allocs/op
+BenchmarkQueryColumnMacro-8 300000 5033 ns/op 8256 B/op 3 allocs/op
+
+BenchmarkQueryColumnMapHand-8 20000 62174 ns/op 147489 B/op 2 allocs/op
+BenchmarkQueryColumnMapMacro-8 20000 62215 ns/op 147568 B/op 4 allocs/op
+
+BenchmarkQueryCombineHand-8 50000 23138 ns/op 65536 B/op 1 allocs/op
+BenchmarkQueryCombineMacro-8 50000 34255 ns/op 65632 B/op 4 allocs/op
+
+BenchmarkQueryGroupHand-8 10000 187355 ns/op 195104 B/op 1003 allocs/op
+BenchmarkQueryGroupMacro-8 20000 92391 ns/op 155866 B/op 11 allocs/op
+
+BenchmarkQueryJoinHand-8 10000 205800 ns/op 248041 B/op 1031 allocs/op
+BenchmarkQueryJoinMacro-8 10000 127462 ns/op 157074 B/op 18 allocs/op
+
+BenchmarkQuerySelectHand-8 500000 3491 ns/op 1024 B/op 1 allocs/op
+BenchmarkQuerySelectMacro-8 300000 5654 ns/op 1088 B/op 3 allocs/op
+
+BenchmarkQuerySortHand-8 2000 788994 ns/op 57488 B/op 4 allocs/op
+BenchmarkQuerySortMacro-8 3000 426857 ns/op 57536 B/op 8 allocs/op
+
+BenchmarkQueryWhereHand-8 100000 15417 ns/op 57344 B/op 1 allocs/op
+BenchmarkQueryWhereMacro-8 100000 19301 ns/op 57408 B/op 3 allocs/op
+
+PASS
+ok github.com/donnol/tools/cmd/gen/testdata 28.909s
+```
--- cmd/gen/testdata/data_test.go
+++ cmd/gen/testdata/data_test.go
@@ -1,6 +1,8 @@
 package testdata
 
 import (
+	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/donnol/tools/query"
@@ -24,5 +26,282 @@ func BenchmarkQueryColumnMacro(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i != b.N; i++ {
 		query.Column(data, "UserId")
+	}
+}
+
+func initQueryColumnMapData() []User {
+	data := make([]User, 1000, 1000)
+	for i := range data {
+		data[i].UserId = rand.Int()
+		data[i].Age = rand.Int()
+	}
+	return data
+}
+
+func BenchmarkQueryColumnMapHand(b *testing.B) {
+	data := initQueryColumnMapData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		newData := make(map[int]User, len(data))
+		for _, single := range data {
+			newData[single.UserId] = single
+		}
+	}
+}
+
+func BenchmarkQueryColumnMapMacro(b *testing.B) {
+	data := initQueryColumnMapData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.ColumnMap(data, "UserId")
+	}
+}
+
+func initQueryCombineData() ([]User, []Admin) {
+	user := make([]User, 1000, 1000)
+	admin := make([]Admin, 1000, 1000)
+	for i := range user {
+		user[i].UserId = rand.Int()
+		user[i].Age = rand.Int()
+		admin[i].AdminId = rand.Int()
+	}
+	return user, admin
+}
+
+func BenchmarkQueryCombineHand(b *testing.B) {
+	users, admins := initQueryCombineData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		result := make([]AdminUser, len(users), len(users))
+		for i := range users {
+			result[i] = AdminUser{
+				AdminId:    admins[i].AdminId,
+				Level:      admins[i].Level,
+				Name:       users[i].Name,
+				CreateTime: users[i].CreateTime,
+			}
+		}
+	}
+}
+
+func BenchmarkQueryCombineMacro(b *testing.B) {
+	user, admin := initQueryCombineData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.Combine(admin, user, func(admin Admin, user User) AdminUser {
+			return AdminUser{
+				AdminId:    admin.AdminId,
+				Level:      admin.Level,
+				Name:       user.Name,
+				CreateTime: user.CreateTime,
+			}
+		})
+	}
+}
+
+func initQueryGroupData() []User {
+	data := make([]User, 1000, 1000)
+	for i := range data {
+		data[i].UserId = rand.Int()
+		data[i].Age = rand.Int()
+	}
+	return data
+}
+
+func BenchmarkQueryGroupHand(b *testing.B) {
+	data := initQueryGroupData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		findMap := make(map[int][]User, len(data))
+		result := make([]Department, 0, len(data))
+		for _, single := range data {
+			users, isExist := findMap[single.UserId]
+			if isExist == false {
+				users = []User{}
+			}
+			users = append(users, single)
+			findMap[single.UserId] = users
+		}
+		for _, single := range data {
+			users, isExist := findMap[single.UserId]
+			if isExist {
+				continue
+			}
+			delete(findMap, single.UserId)
+			result = append(result, Department{
+				Employees: users,
+			})
+		}
+	}
+}
+
+func BenchmarkQueryGroupMacro(b *testing.B) {
+	data := initQueryGroupData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.Group(data, "UserId", func(users []User) Department {
+			return Department{
+				Employees: users,
+			}
+		})
+	}
+}
+
+func initQueryJoinData() ([]User, []Admin) {
+	user := make([]User, 1000, 1000)
+	admin := make([]Admin, 1000, 1000)
+	for i := range user {
+		user[i].UserId = rand.Int()
+		user[i].Age = rand.Int()
+		admin[i].AdminId = rand.Int()
+	}
+	return user, admin
+}
+
+func BenchmarkQueryJoinHand(b *testing.B) {
+	users, admins := initQueryJoinData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		adminMap := map[int][]Admin{}
+		for _, admin := range admins {
+			findAdmins, isExist := adminMap[admin.AdminId]
+			if isExist == false {
+				findAdmins = []Admin{}
+			}
+			findAdmins = append(findAdmins, admin)
+			adminMap[admin.AdminId] = findAdmins
+		}
+		result := make([]AdminUser, 0, len(users))
+		for _, user := range users {
+			admins := adminMap[user.UserId]
+			for _, admin := range admins {
+				result = append(result, AdminUser{
+					AdminId:    admin.AdminId,
+					Level:      admin.Level,
+					Name:       user.Name,
+					CreateTime: user.CreateTime,
+				})
+			}
+		}
+	}
+}
+
+func BenchmarkQueryJoinMacro(b *testing.B) {
+	user, admin := initQueryJoinData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.LeftJoin(admin, user, "AdminId = UserId", func(admin Admin, user User) AdminUser {
+			return AdminUser{
+				AdminId:    admin.AdminId,
+				Level:      admin.Level,
+				Name:       user.Name,
+				CreateTime: user.CreateTime,
+			}
+		})
+	}
+}
+
+func BenchmarkQuerySelectHand(b *testing.B) {
+	data := make([]User, 1000, 1000)
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		newData := make([]Sex, len(data), len(data))
+		for i, single := range data {
+			newData[i] = func(a User) Sex {
+				if len(a.Name) >= 3 && a.Name[0:3] == "Man" {
+					return Sex{IsMale: true}
+				}
+				return Sex{IsMale: false}
+			}(single)
+		}
+	}
+}
+
+func BenchmarkQuerySelectMacro(b *testing.B) {
+	data := make([]User, 1000, 1000)
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.Select(data, func(a User) Sex {
+			if len(a.Name) >= 3 && a.Name[0:3] == "Man" {
+				return Sex{IsMale: true}
+			}
+			return Sex{IsMale: false}
+		})
+	}
+}
+
+func initQuerySortData() []User {
+	data := make([]User, 1000, 1000)
+	for i := range data {
+		data[i].UserId = rand.Int()
+		data[i].Age = rand.Int()
+	}
+	return data
+}
+
+func BenchmarkQuerySortHand(b *testing.B) {
+	data := initQuerySortData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		newData := make([]User, len(data), len(data))
+		copy(newData, data)
+		sort.SliceStable(newData, func(i int, j int) bool {
+			return newData[i].UserId < newData[j].UserId
+		})
+	}
+}
+
+func BenchmarkQuerySortMacro(b *testing.B) {
+	data := initQuerySortData()
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.Sort(data, "UserId asc")
+	}
+}
+
+func BenchmarkQueryWhereHand(b *testing.B) {
+	data := make([]User, 1000, 1000)
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		newData := make([]User, 0, len(data))
+		for _, single := range data {
+			isMan := func(a User) bool {
+				if len(a.Name) >= 3 && a.Name[0:3] == "Man" {
+					return true
+				}
+				return false
+			}(single)
+
+			if isMan {
+				newData = append(newData, single)
+			}
+		}
+	}
+}
+
+func BenchmarkQueryWhereMacro(b *testing.B) {
+	data := make([]User, 1000, 1000)
+
+	b.ResetTimer()
+	for i := 0; i != b.N; i++ {
+		query.Where(data, func(a User) bool {
+			if len(a.Name) >= 3 && a.Name[0:3] == "Man" {
+				return true
+			}
+			return false
+		})
 	}
 }
